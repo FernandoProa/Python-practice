6529bda1d8b63b385d14c0b7c082c5eb178d289f
fernando proa <45346347+FernandoProa@users.noreply.github.com>
2022-04-23 20:36:02 -0500

    basics input and print

---
+++ 01 basics/main.py
@@ -0,0 +1,7 @@
+# This is a sample Python script.
+
+# Press Mayús+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+print("El nombre de tu cerveza es \n \"" + input('Piensa en un animal: ') + " " + input('Piensa en un lugar: ') + "\"")
+# print("Tu apellido es: " + input('Ingresa tu apellido: '))
