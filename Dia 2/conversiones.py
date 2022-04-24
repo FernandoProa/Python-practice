428d3cf762a3d526e6b05249b40c45bba22cf7de
fernando proa <45346347+FernandoProa@users.noreply.github.com>
2022-04-24 11:10:34 -0500

    Manejo numerico de datos, conversiones y concatenacion a cadenas

---
+++ Dia 2/conversiones.py
@@ -0,0 +1,31 @@
+
+
+
+
+
+#Conversiones explicitas
+# num1 = 5.8
+#
+# print(num1)
+# print(type(num1))
+#
+# num2 = int(num1)
+#
+# print(num2)
+# print(type(num2))
+#
+# edad = input('Dime tu edad ')
+# print(type(edad))
+# edad = int(edad)
+# nueva_edad = 1 + edad
+# print(nueva_edad)
+
+
+# Conversiones implicitas
+# num1 = 20
+# num2 = 30.5
+#
+# num1 = num1 + num2
+#
+# print(type(num1))
+# print(type(num2))
---
+++ Dia 2/formatear_cadenas.py
@@ -0,0 +1,11 @@
+color = 'rojo'
+
+matricula = 541926
+
+print(f'El auto es { color } y su matricula es { matricula }')
+
+
+# x = 10
+# y = 5
+#
+# print(f'Mis numeros son: {x} y {y}, la suma de ambos es { x + y }')
